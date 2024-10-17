9a6d8a3b3c340017d1d8f5d93642beeeea51d874
hinkleydev <azz@hinkley.dev>
2024-10-17 10:27:06 +0100

    basic light up done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Write your code here
+/** Toggles the 'active' class on an element
+ * 
+ * @param {HTMLDivElement} object The HTML to toggle the class on
+ */
+function toggleBulb(object) {
+    object.classList.toggle("active")
+}
